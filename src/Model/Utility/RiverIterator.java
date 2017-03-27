e5244386187d7b73ba6a8abc0794c863e6d8e09a
Cameron Jeffords <camj@ufl.edu>
2017-03-27 19:56:09 -0400

    Basic iterators made, no tests

---
+++ src/Model/Utility/RiverIterator.java
@@ -0,0 +1,78 @@
+package Model.Utility;
+
+import Model.Tile.BuildTile;
+import Model.Tile.BuildTileFactory;
+import Model.Tile.Tile;
+import Model.Visitor.TileDrawingVisitor;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+
+public class RiverIterator implements TileIterator {
+    final int[][] riverIndexList = { { }, { 1 }, { 1, 2 }, { 1, 3 }, {3, 6}, {1, 3, 5} };
+    ArrayList<BuildTile> tileList;
+    private BuildTile selectedTile;
+    private String terrain;
+    BuildTileFactory factory;
+    int currentIndex;
+    TileDrawingVisitor tdv;
+
+    public RiverIterator(String terrain){
+        this.terrain = terrain;
+        factory = new BuildTileFactory();
+        tileList = new ArrayList<>();
+        tdv = new TileDrawingVisitor();
+        for (int[] ints : riverIndexList) {
+            tileList.add(
+                    factory.createTile(terrain, ints)
+            );
+        }
+        selectedTile = tileList.get(0).clone();
+    }
+
+    @Override
+    public void first() {
+        currentIndex = 0;
+    }
+
+    @Override
+    public void next() {
+        currentIndex++;
+        currentIndex %= riverIndexList.length;
+    }
+
+    @Override
+    public Tile current() {
+        return tileList.get(currentIndex);
+    }
+
+    @Override
+    public BufferedImage getImage() {
+        tileList.get(currentIndex).accept(tdv);
+        return tdv.getImage();
+    }
+
+    @Override
+    public void rotate() {
+        selectedTile.rotate();
+    }
+
+    @Override
+    public TileIterator getRiverIterator() {
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return riverIndexList.length;
+    }
+
+    public void setSelectedTile(int index){
+        selectedTile = tileList.get(index).clone();
+    }
+
+    public Tile getSelectedTile(){
+        return selectedTile;
+    }
+}
---
+++ src/Model/Utility/TerrainIterator.java
@@ -0,0 +1,64 @@
+package Model.Utility;
+
+import Model.Tile.BuildTile;
+import Model.Tile.BuildTileFactory;
+import Model.Tile.Tile;
+import Model.Visitor.TileDrawingVisitor;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class TerrainIterator implements TileIterator {
+    final String[] terrainList = {"WOODS", "PLAINS", "MOUNTAIN", "PASTURE", "ROCK", "SEA"};
+    BuildTileFactory factory;
+    ArrayList<BuildTile> tileList;
+    int currentIndex;
+    TileDrawingVisitor tdv;
+
+    public TerrainIterator(){
+        factory = new BuildTileFactory();
+        tileList = new ArrayList<>();
+        tdv = new TileDrawingVisitor();
+        for (String s : terrainList) {
+            tileList.add(
+                    factory.createTile(s, new int[]{})
+            );
+        }
+    }
+
+    @Override
+    public void first() {
+        currentIndex = 0;
+    }
+
+    @Override
+    public void next() {
+        currentIndex++;
+        currentIndex %= terrainList.length;
+    }
+
+    @Override
+    public Tile current() {
+        return tileList.get(currentIndex);
+    }
+
+    @Override
+    public BufferedImage getImage() {
+        tileList.get(currentIndex).accept(tdv);
+        return tdv.getImage();
+    }
+
+    @Override
+    public void rotate() {
+        //Do nothing
+    }
+
+    @Override
+    public TileIterator getRiverIterator() {
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return terrainList.length;
+    }
+}
