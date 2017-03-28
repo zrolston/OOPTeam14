b539d66444012571c733365f5ab8af6a493eddde
WilliamWickerson <willie7296@gmail.com>
2017-03-28 17:12:55 -0400

    Added save/load buttons

--- src/Views/MapEditor/MapEditorView.java
+++ src/Views/MapEditor/MapEditorView.java
@@ -17,6 +17,7 @@ public class MapEditorView extends JLayeredPane {
     private TileSelectionView tileSelectionView;
     private MapSubsectionView mapSubsectionView;
     private ScaleSliderView scaleSliderView;
+    private SaveLoadView saveLoadView;
 
     public MapEditorView()  {
         setBounds(0, 0, PixelMap.SCREEN_WIDTH, PixelMap.SCREEN_HEIGHT);
@@ -25,11 +26,13 @@ public class MapEditorView extends JLayeredPane {
         tileSelectionView = new TileSelectionView();
         mapSubsectionView = new MapSubsectionView();
         scaleSliderView   = new ScaleSliderView( mapSubsectionView );
+        saveLoadView      = new SaveLoadView();
 
         // Add SubViews to Layered Pane
         add(tileSelectionView, new Integer(3));
         add(mapSubsectionView, new Integer(1));
         add(scaleSliderView, new Integer(2));
+        add(saveLoadView, new Integer(2));
     }
 
     public TileSelectionView getTileSelectionView() {
