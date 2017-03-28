82ba630211a1fa1f86d59e01e2ea743d4fa6b773
WilliamWickerson <willie7296@gmail.com>
2017-03-28 17:45:47 -0400

    Added button file select and fixed TileSelectionController being slightly off

--- src/Controllers/ButtonListener/SaveButtonListener.java
+++ src/Controllers/ButtonListener/SaveButtonListener.java
@@ -1,7 +1,9 @@
 package Controllers.ButtonListener;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 /**
  * Created by Willie on 3/28/2017.
@@ -10,6 +12,13 @@ public class SaveButtonListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("Save");
+        JFileChooser chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new File("."));
+        chooser.setVisible(true);
+        File file = null;
+        if (chooser.showOpenDialog(chooser) == JFileChooser.APPROVE_OPTION) {
+            file = chooser.getSelectedFile();
+            //TODO: save map
+        }
     }
 }
