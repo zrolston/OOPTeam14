36f2addf8a88975d08d2d62d8aef7ef1351e3b59
Randy Brooks <randygeraldbrooks2@gmail.com>
2017-04-14 21:01:19 -0400

    Wonder Screen

--- src/Gameplay/Views/WonderView.java
+++ src/Gameplay/Views/WonderView.java
@@ -1,12 +1,17 @@
 package Gameplay.Views;
 
 import javax.swing.*;
+
+import MapBuilder.Views.Utility.ImageLoader;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 
 public class WonderView extends JPanel {
-
+	BufferedImage wonder = ImageLoader.getImage("WONDER");
+	BufferedImage wonderBackground = ImageLoader.getImage("WONDER_BACKGROUND");
     ScreenSelectButtons screenSelectBtns;
     Display display;
 
@@ -18,7 +23,15 @@ public class WonderView extends JPanel {
         setBackground( new Color( 0xffa7e163 ) );
         setVisible(true);
     }
-
+    
+    @Override
+    public void paintComponent(Graphics g) {
+    	((Graphics2D)(g)).setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+    	int height = (int)(getHeight() * 0.85);
+    	int width = (834 * height)/906;
+    	g.drawImage(wonderBackground, 0 , 0, getWidth(), getHeight(), null);
+    	g.drawImage(wonder, this.getWidth()/2 - width/2 , (int)(getHeight() * 0.1), width, height, null);
+    }
 
     public void addCustomListenersToScreenSelectBtns() {
 
