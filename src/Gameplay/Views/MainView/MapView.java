53bed8d0aef73511d78630c761c19280bd3a8361
thomas1242 <thomaspalmer121@gmail.com>
2017-04-15 01:46:31 -0400

    add next phase button to mainview

--- src/Gameplay/Views/MainView/MapView.java
+++ src/Gameplay/Views/MainView/MapView.java
@@ -1,9 +1,20 @@
 package Gameplay.Views.MainView;
 
+import MapBuilder.Views.Utility.PixelMap;
+
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Willie on 4/14/2017.
  */
 public class MapView extends JPanel {
+
+    public MapView() {
+        setBounds(0, 0, (int)(PixelMap.SCREEN_WIDTH ), (int)(PixelMap.SCREEN_HEIGHT ));
+        setBackground( new Color( 0xff9de7d7 ) );
+        setVisible(true);
+    }
+
+
 }
