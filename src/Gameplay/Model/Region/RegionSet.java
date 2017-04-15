4a82e50e151ea592612a10678862bf55f576be7e
zrolston <zrolston@ufl.edu>
2017-04-15 18:38:31 -0400

    added port regions to the RegionSet

--- src/Gameplay/Model/Region/RegionSet.java
+++ src/Gameplay/Model/Region/RegionSet.java
@@ -8,6 +8,7 @@ public class RegionSet {
     private List<Region> roadRegions;
     private List<Region> waterRegions;
     private List<Region> dockRegions;
+    private List<Region> portRegions;
 
     public List<Region> getLandRegions() {
         return landRegions;
@@ -20,6 +21,9 @@ public class RegionSet {
     }
     public List<Region> getDockRegions() {
         return dockRegions;
+    }
+    public List<Region> getPortRegions() {
+        return portRegions;
     }
 
     public RegionSet(){
@@ -73,6 +77,17 @@ public class RegionSet {
     }
     public void removeDockRegion(Region dockRegion) {
         this.dockRegions.remove(dockRegion);
+    }
+
+    public void addPortRegion(Region portRegion) {
+
+        if(!this.portRegions.contains(portRegion)) {
+            this.portRegions.add(portRegion);
+        }
+
+    }
+    public void removePortRegion(Region portRegion) {
+        this.portRegions.remove(portRegion);
     }
 
 
