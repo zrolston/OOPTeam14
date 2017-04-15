7904d492f1f3114b129b1efff22a8f5fbb27a860
Randy Brooks <randygeraldbrooks2@gmail.com>
2017-04-14 22:39:14 -0400

    Wonder Bricks

--- src/Gameplay/Views/WonderView.java
+++ src/Gameplay/Views/WonderView.java
@@ -8,13 +8,11 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.util.Random;
 
 public class WonderView extends JPanel {
 	BufferedImage wonder = ImageLoader.getImage("WONDER");
 	BufferedImage wonderBackground = ImageLoader.getImage("WONDER_BACKGROUND");
-	BufferedImage blueBrick = ImageLoader.getImage("BLUE_BRICK");
-	BufferedImage orangeBrick = ImageLoader.getImage("ORANGE_BRICK");
-	BufferedImage neutralBrick = ImageLoader.getImage("NEUTRAL_BRICK");
     ScreenSelectButtons screenSelectBtns;
     Display display;
 
@@ -29,21 +27,36 @@ public class WonderView extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
     	((Graphics2D)(g)).setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-    	
     	int height = (int)(getHeight() * 0.85);
     	int width = (819 * height)/901;
     	int x = getWidth()/2 - width/2;
     	int y = (int)(getHeight() * 0.1);
     	g.drawImage(wonderBackground, 0 , 0, getWidth(), getHeight(), null);
     	g.drawImage(wonder, x, y, width, height, null);
-    	drawBrick(g, 1, (double)width/(double)819);
+    	//drawBrick(g, position (0-61), "BLUE", x, y, width, height);
+    	//drawBrick(g, position (0-61), "ORANGE", x, y, width, height);
+    	//drawBrick(g, position (0-61), "NEUTRAL", x, y, width, height);
     }
 
-    private void drawBrick(Graphics g, int position, double scale) {
-    	int x = 500;
-    	int y = 500;
-    	
-    	g.drawImage(blueBrick, x, y, (int)(blueBrick.getWidth() * scale), (int)(blueBrick.getHeight() * scale), null);
+    private void drawBrick(Graphics g, int position, String color, int x, int y, int width, int height) {
+    	int level = 0; //This is the level from the bottom of the wonder that the brick is on
+    	int brickX = 0;
+    	double scale = (double)width/(double)819;
+    	if (position <= 11) {
+    		level = position / 4;
+    		brickX = (x + (int)(width/3.45) + (int)(width/9.4 * (position % 4)));
+    	} else if (position <= 31) {
+    		level = ((position - 12) / 5) + 3;
+    		brickX = (x + (int)(width/4.18) + (int)(width/9.4 * ((position - 12) % 5)));
+    	} else {
+    		level = ((position - 32) / 6) + 7;
+    		brickX = (x + (int)(width/5.52) + (int)(width/9.4 * ((position - 32) % 6)));
+    	}
+    	int brickY = (y + (int)(height * (0.85 - (level * 0.051))));
+    	BufferedImage brick = ImageLoader.getImage(color + "_BRICK");
+    	int brickWidth = (int)(brick.getWidth() * scale);
+    	int brickHeight = (int)(brick.getHeight() * scale);
+    	g.drawImage(brick, brickX, brickY, brickWidth, brickHeight, null);
 	}
 
 	public void addCustomListenersToScreenSelectBtns() {
