344c009efd2bca1df649d1d43d34b46dc01e2abb
WilliamWickerson <willie7296@gmail.com>
2017-04-16 00:49:09 -0400

    Added screen scrolling for map tiles

--- src/Gameplay/Views/MainView/MapView.java
+++ src/Gameplay/Views/MainView/MapView.java
@@ -2,26 +2,21 @@ package Gameplay.Views.MainView;
 
 import Gameplay.Controller.CameraController;
 import Gameplay.Model.Visitors.GameMapDrawingVisitor;
-import Gameplay.Model.Visitors.GameTileDrawingVisitor;
-import Gameplay.Views.Utility.PolygonUtility;
 import Gameplay.Views.Utility.PixelMap;
 import Gameplay.Views.Utility.RenderingThread;
 import MapBuilder.Model.Map.IViewMap;
 import MapBuilder.Model.Utility.HexLocation;
-import MapBuilder.Model.Utility.ILocation;
-import MapBuilder.Views.Drawers.TileInternalDrawer;
+import Gameplay.Views.Drawers.TileInternalDrawer;
 import MapBuilder.Views.Utility.PixelPoint;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-
 public class MapView extends JPanel {
 
     private RenderingThread renderingThread;
-    private ILocation[][] map;
     private BufferedImage[][] tileImages;
-    private int size = 50;
 
     public void updateTileImages(IViewMap map) {
         GameMapDrawingVisitor drawingVisitor = new GameMapDrawingVisitor();
@@ -30,12 +25,6 @@ public class MapView extends JPanel {
     }
 
     public MapView(){
-        map = new HexLocation[size][size];
-        for(int i=0; i<size; i++) {
-            for (int j = 0; j < size; j++) {
-                map[i][j] = new HexLocation(i,j);
-            }
-        }
         setLayout(null);
         setBounds(0,0,PixelMap.SCREEN_WIDTH, PixelMap.SCREEN_HEIGHT);
         setVisible(true);
@@ -49,23 +38,15 @@ public class MapView extends JPanel {
     }
 
     @Override
-    public void paintComponent(Graphics g){
-
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        for(int i=0; i<size; i++){
-            for(int j=0; j<size; j++){
-                PixelPoint center = PixelMap.getMapTileOrigin(map[i][j]);
-                Polygon hexaTile = PolygonUtility.getHexagon(center);
-                g.drawPolygon(hexaTile);
-            }
-        }
-
+        
         if (tileImages == null)
             return;
         for (int i = 0; i < tileImages.length; i++) {
             for (int j = 0; j < tileImages[i].length; j++) {
-                if (MapBuilder.Views.Utility.PixelMap.isTileVisible(new HexLocation(i, j))) {
-                    PixelPoint origin = MapBuilder.Views.Utility.PixelMap.getMapTileOrigin(new HexLocation(i, j));
+                if (PixelMap.isTileVisible(new HexLocation(i, j))) {
+                    PixelPoint origin = PixelMap.getMapTileOrigin(new HexLocation(i,j));
                     TileInternalDrawer.drawInMap(g, tileImages[i][j], origin);
                 }
             }
