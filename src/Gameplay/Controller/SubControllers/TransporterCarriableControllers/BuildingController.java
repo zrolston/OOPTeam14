49323d4154107070cb2f7dcb45adb2a7d2234d90
jordiahl <jordiahl@ufl.edu>
2017-04-17 09:26:13 -0400

    somethin4

--- src/Gameplay/Controller/SubControllers/TransporterCarriableControllers/BuildingController.java
+++ src/Gameplay/Controller/SubControllers/TransporterCarriableControllers/BuildingController.java
@@ -1,11 +1,13 @@
 package Gameplay.Controller.SubControllers.TransporterCarriableControllers;
 
 import Gameplay.Controller.MainViewController;
+import Gameplay.Model.BuildAbilities.BuildAbility;
 import Gameplay.Model.Utility.GameModelFacade;
 import Gameplay.Views.MainView.MainView;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.List;
 
 /**
  * Created by jordi on 4/17/2017.
@@ -15,6 +17,8 @@ public class BuildingController implements KeyListener, MainViewController {
     MainView mainView;
     boolean isPSVOn = false;
     boolean isWSVOn = false;
+    GameModelFacade gameModelFacade = GameModelFacade.getInstance();
+
     @Override
     public void keyTyped(KeyEvent e) {
 
@@ -22,12 +26,16 @@ public class BuildingController implements KeyListener, MainViewController {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_B && mainView!= null){
+        if (e.getKeyCode() == KeyEvent.VK_B && mainView != null) {
             isPSVOn = !isPSVOn;
             mainView.getProducerSelectionView().setVisible(isPSVOn);
-            mainView.getProducerSelectionView().updateProducerImages(GameModelFacade.getInstance().getBuildAbilities(userRequestController.getRegion()));
+
+            if (userRequestController.getRegion() != null) {
+                List<BuildAbility> abilities = gameModelFacade.getBuildAbilities(userRequestController.getRegion());
+                mainView.getProducerSelectionView().updateProducerImages(abilities);
+            }
         }
-        if (e.getKeyCode() == KeyEvent.VK_W && mainView!= null){
+        if (e.getKeyCode() == KeyEvent.VK_W && mainView != null) {
             isWSVOn = !isWSVOn;
             mainView.getWallSelectionView().setVisible(isWSVOn);
         }
@@ -47,7 +55,7 @@ public class BuildingController implements KeyListener, MainViewController {
 
     @Override
     public void activateController(MainView mainView) {
-        this.mainView= mainView;
+        this.mainView = mainView;
         userRequestController.activateController(mainView);
         mainView.getInputSelectionView().setVisible(true);
         mainView.getDisplay().setFocusable(true);
