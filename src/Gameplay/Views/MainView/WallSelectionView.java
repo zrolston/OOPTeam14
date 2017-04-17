6fbcab40c193aa106ad64ac2b9a769f2a71664ae
thomas1242 <thomaspalmer121@gmail.com>
2017-04-17 04:26:34 -0400

    wall button

--- src/Gameplay/Views/MainView/WallSelectionView.java
+++ src/Gameplay/Views/MainView/WallSelectionView.java
@@ -34,12 +34,12 @@ public class WallSelectionView extends JPanel {
         ArrayList<JButton> btns = new ArrayList<>();
         ArrayList<String> strings = new ArrayList<>();
 
-        strings.add("NE");
+        strings.add("NW");
         strings.add("N");
-        strings.add("W");
-        strings.add("SE");
-        strings.add("S");
+        strings.add("NE");
         strings.add("SW");
+        strings.add("S");
+        strings.add("SE");
 
         for (int i = 0; i < strings.size(); i++) {
             JButton button = new JButton( strings.get(i) );
